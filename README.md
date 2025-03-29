603239218e41aa10e3ba8b6a76f22a701dcba1b2
Heinrich Alexandra Hermann <github@dev.hermann-bsd.de>
2025-03-29 12:00:34 +0100

    PHP.ini Manager initial commit
    This is the initial commit for the open source project for a plugin on
    the JetBrains IDE platform.
    
    This project is for a simple editor of project related php.ini files.

---
+++ README.md
@@ -0,0 +1,2 @@
+# PHP.ini Manager 1.0
+Plugin for JetBrains IDE to manage `php.ini` files in projects.
---
+++ src/main/kotlin/de/hermannbsd/phpinimanager/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Package for the IDE plugin PHP.ini Manager
+ *
+ * @version 1.0
+ * @author Heinrich Alexandra Hermann a.hermann@dev.hermann-bsd.de
+ */
+package de.hermannbsd.phpinimanager;
